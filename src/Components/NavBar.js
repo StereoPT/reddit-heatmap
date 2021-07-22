6d278fed2b4ac7c9b5c9ba74be44c60d2e8e6b0d
Guido Pereira <stereopt@gmail.com>
2021-07-22 11:53:40 +0100

    NavBar Tailwind.css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,61 +1,48 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logo from '../images/Reddit-icon.png'
 
 function NavBar() {
   return (
-    <Navbar>
-      <ul>
-        <div className="logo">
-          <Link to="/">
-            <img src={logo} alt="logo"/>
+    <Navbar className="p-2 text-white">
+      <div className="w-full mx-auto px-4">
+        <div className="flex flex-wrap items-center justify-center md:justify-start">
+          <Link to="/" className="logo mr-4">
+            <img src={ logo } alt="logo"/>
           </Link>
+          <ul className="flex flex-wrap justify-center text-center">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/howitworks">How It Works</Link>
+            </li>
+            <li>
+              <Link to="/contacts">Contacts</Link>
+            </li>
+          </ul>
         </div>
-        <li>
-          <Link to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/search">
-            Search
-          </Link>
-        </li>
-        <li>
-          <Link to="/about">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/howitworks">
-            How It Works
-          </Link>
-        </li>
-        <li className="mr-16">
-          <Link to="/contacts">
-            Contacts
-          </Link>
-        </li>
-      </ul>
+      </div>
     </Navbar>
   )
 }
 
 const Navbar = styled.nav `
-  background: #3492AE;
-  color: white;
+  background-color: #212529;
+
   .logo{
-    width: 5%;
-  }
-  ul{
-    display: flex;
-    justify-content: space-around;
-  }
-  li{
-    display: flex;
-    justify-content: center ;
-    align-items: center;
+    width: 40px;
+  };
+
+  ul li a {
+    padding: 0.5rem 1rem;
   }
 `;
 
