4b56349eb1b04e58d6952c437726b4dd7ada1405
Guido Pereira <stereopt@gmail.com>
2021-07-27 10:13:48 +0100

    Image Placeholder

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
       <h3 className="text-lg mt-4">You are in the right place! Find the best time to post on your Subreddit.</h3>
       <CallToAction className="my-12 bg-yellow-500 hover:bg-yellow-600 px-6 py-3 rounded-lg text-white text-md font-bold">Show me the Best Time</CallToAction>
       <p className="text-lg text-gray-400">r/javascript</p>
-      <img src="" alt="" />
+      <img className="mx-auto mt-4" src="https://via.placeholder.com/650x200" alt="" />
     </div>
   );
 }
